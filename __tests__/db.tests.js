fbe8b7681cf0ff1eb1bbc92918ce07aab141f29c
Aaron Brown <aaronbrown@MacBook-Pro-2.local>
2024-01-03 14:10:18 -0800

    test two

--- __tests__/db.tests.js
+++ __tests__/db.tests.js
@@ -3,16 +3,23 @@ const path = require('path');
 const db = require('');
 const controller = require('../server/controllers/controller.js')
 
-// test create entry
+// NOTE: testing here uses username 'aaa', password 'xxx'
+
 const testEntry = {
     username: 'aaa',
     bloodSugar: 111,
     sysPressure: 122,
-    disPressure: 88,
+    diaPressure: 88,
 };
 
+let newEntryId;
+let newEntry;
+let resData;
+
+// test create entry
+
 describe('db entries', () => {
-    it('saves a new entry to the db', () => {
+    it('posts a new entry to the db', () => {
         const result = async () => {
         try {
             const response = await fetch('http://localhost:3000/api/entry', {
@@ -22,13 +29,29 @@ describe('db entries', () => {
                   },
                   body: JSON.stringify(testEntry),
                 });
-                const newEntryId = await response.json();
-        }  catch (error){
+            console.log('response in test: ', response);
+            newEntryId = await response.json().id;
+        } catch (error){
             console.log('error in post new entry test')
             }
         }
         expect(result).not.toBeInstanceOf(Error);
+        expect(newEntryId).toBeInstanceOf(String);
     });
+    xit('retrieves the new entry from the db', () => {
+        const result = async () => {
+            try {
+                const response = await fetch('hhtp://localhost:3000/api/homepage/bloodsugar');
+                resData = await response.json();
+            } catch (error) {
+                console.log('error in get new entry test')
+            }
+        };
+        newEntry = resData.filter(el => el._id === newEntryId);
+        expect(result).not.toBeInstanceOf(Error);
+        expect(newEntry).toBeTruthy();
+    })
 })
 
+// test retrieve entry
 // test delete entry 
